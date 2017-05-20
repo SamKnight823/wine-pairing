f8fbc37dd0666d49b066a33d994b95227261253f
Samantha Knight <sammay82@yahoo.com>
2017-05-20 17:11:52 +0000

    added scrape template

--- lib/wine-pairing/wine.rb
+++ lib/wine-pairing/wine.rb
@@ -1,4 +1,4 @@
-Class WinePairing :: WinePairing
+Class WinePairing :: Wine
   attr_accessor :name, :description, :taste
 
   def self.list
@@ -13,5 +13,40 @@ Class WinePairing :: WinePairing
     wines << self.scrape_versatile
     wines
   end
-    
+
+    def self.scrape_red
+      doc = Nokogiri::HTML(open("https://www.realsimple.com/holidays-entertaining/entertaining/food-drink/basic-wine-varieties"))
+
+      wine = self.new
+      wine.name = 
+      wine.description = 
+      wine.taste = 
+    end
+
+    def self.scrape_white
+      doc = Nokogiri::HTML(open("https://www.realsimple.com/holidays-entertaining/entertaining/food-drink/basic-wine-varieties"))
+
+      wine = self.new
+      wine.name = 
+      wine.description = 
+      wine.taste = 
+    end
+
+    def self.scrape_light
+      doc = Nokogiri::HTML(open("https://www.realsimple.com/holidays-entertaining/entertaining/food-drink/basic-wine-varieties"))
+  
+      wine = self.new
+      wine.name = 
+      wine.description = 
+      wine.taste = 
+    end
+
+    def self.scrape_versatile
+      doc = Nokogiri::HTML(open("https://www.realsimple.com/holidays-entertaining/entertaining/food-drink/basic-wine-varieties"))
+
+      wine = self.new
+      wine.name = 
+      wine.description = 
+      wine.taste = 
+    end    
 end
